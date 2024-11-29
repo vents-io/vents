28f0d5c4fe36a5f9dbfc271f37ffab6db560f018
mouradmourafiq <mouradmourafiq@gmail.com>
2024-11-29 12:57:36 +0100

    Add reddit provider

---
+++ vents/tests/test_providers/test_reddit/test_service.py
@@ -0,0 +1,142 @@
+import os
+
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from vents.config import AppConfig
+from vents.providers.reddit.service import RedditRssService, RedditService
+
+
+class TestRedditService(TestCase):
+    def setUp(self):
+        self.client_id = "test-client-id"
+        self.client_secret = "test-client-secret"
+        self.user_agent = "test-user-agent"
+        self.username = "test-username"
+        self.password = "test-password"
+
+    def test_init(self):
+        service = RedditService(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            user_agent=self.user_agent,
+            username=self.username,
+            password=self.password,
+        )
+        self.assertEqual(service.client_id, self.client_id)
+        self.assertEqual(service.client_secret, self.client_secret)
+        self.assertEqual(service.user_agent, self.user_agent)
+        self.assertEqual(service.username, self.username)
+        self.assertEqual(service.password, self.password)
+
+    @patch.object(AppConfig, "read_keys")
+    def test_load_from_connection_with_secret(self, mock_read_keys):
+        mock_read_keys.side_effect = [
+            self.client_id,
+            self.client_secret,
+            self.user_agent,
+            self.username,
+            self.password,
+        ]
+
+        mock_connection = MagicMock()
+        mock_connection.secret.mount_path = "/path/to/secret"
+        mock_connection.schema = None
+        mock_connection.env = None
+        mock_connection.config_map = None
+
+        service = RedditService.load_from_connection(connection=mock_connection)
+
+        self.assertEqual(service.client_id, self.client_id)
+        self.assertEqual(service.client_secret, self.client_secret)
+        self.assertEqual(service.user_agent, self.user_agent)
+        self.assertEqual(service.username, self.username)
+        self.assertEqual(service.password, self.password)
+
+    @patch.object(AppConfig, "read_keys")
+    def test_load_from_connection_without_connection(self, mock_read_keys):
+        mock_read_keys.side_effect = [
+            self.client_id,
+            self.client_secret,
+            self.user_agent,
+            self.username,
+            self.password,
+        ]
+
+        service = RedditService.load_from_connection(connection=None)
+
+        self.assertEqual(service.client_id, self.client_id)
+        self.assertEqual(service.client_secret, self.client_secret)
+        self.assertEqual(service.user_agent, self.user_agent)
+        self.assertEqual(service.username, self.username)
+        self.assertEqual(service.password, self.password)
+
+    @patch("praw.Reddit")
+    def test_set_session(self, mock_reddit):
+        service = RedditService(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            user_agent=self.user_agent,
+            username=self.username,
+            password=self.password,
+        )
+        service._set_session()
+
+        mock_reddit.assert_called_once_with(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            user_agent=self.user_agent,
+            username=self.username,
+            password=self.password,
+        )
+
+    def test_set_env_vars(self):
+        service = RedditService(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            user_agent=self.user_agent,
+            username=self.username,
+            password=self.password,
+        )
+        service.set_env_vars()
+
+        self.assertEqual(os.environ.get("REDDIT_CLIENT_ID"), self.client_id)
+        self.assertEqual(os.environ.get("REDDIT_CLIENT_SECRET"), self.client_secret)
+        self.assertEqual(os.environ.get("REDDIT_USER_AGENT"), self.user_agent)
+        self.assertEqual(os.environ.get("REDDIT_USERNAME"), self.username)
+        self.assertEqual(os.environ.get("REDDIT_PASSWORD"), self.password)
+
+
+class TestRedditRssService(TestCase):
+    def setUp(self):
+        self.url = "https://reddit.com/r/test.rss"
+        self.default_url = "https://reddit.com/r/{subreddit}.rss"
+
+    def test_init(self):
+        service = RedditRssService(url=self.url)
+        self.assertEqual(service.url, self.url)
+
+    @patch.object(AppConfig, "read_keys")
+    def test_load_from_connection_with_secret(self, mock_read_keys):
+        mock_read_keys.return_value = self.url
+
+        mock_connection = MagicMock()
+        mock_connection.secret.url = "/path/to/url"
+
+        service = RedditRssService.load_from_connection(connection=mock_connection)
+
+        self.assertEqual(service.url, self.url)
+        mock_read_keys.assert_called_once_with(
+            context_paths=["/path/to/url"], keys=["REDDIT_RSS_URL"]
+        )
+
+    @patch.object(AppConfig, "read_keys")
+    def test_load_from_connection_without_connection(self, mock_read_keys):
+        mock_read_keys.return_value = None
+
+        service = RedditRssService.load_from_connection(connection=None)
+
+        self.assertEqual(service.url, self.default_url)
+        mock_read_keys.assert_called_once_with(
+            context_paths=[], keys=["REDDIT_RSS_URL"]
+        )
---
+++ vents/vents/providers/reddit/service.py
@@ -0,0 +1,117 @@
+import os
+
+from typing import TYPE_CHECKING, Optional
+
+from vents.providers.base import BaseHttpService, BaseService
+from vents.settings import VENTS_CONFIG
+
+if TYPE_CHECKING:
+    from vents.connections.connection import Connection
+
+
+class RedditService(BaseService):
+    client_id: Optional[str]
+    client_secret: Optional[str]
+    user_agent: Optional[str]
+    username: Optional[str]
+    password: Optional[str]
+
+    @classmethod
+    def load_from_connection(
+        cls, connection: Optional["Connection"]
+    ) -> Optional["RedditService"]:
+        # Check if there are mounting based on secrets/configmaps
+        context_paths = []
+        schema = None
+        builtin_env = None
+        if connection:
+            if connection.secret and connection.secret.mount_path:
+                context_paths.append(connection.secret.mount_path)
+            if connection.config_map and connection.config_map.mount_path:
+                context_paths.append(connection.config_map.mount_path)
+            if connection.schema:
+                schema = connection.schema
+            if connection.env:
+                builtin_env = connection.env
+
+        client_id = VENTS_CONFIG.read_keys(
+            context_paths=context_paths,
+            schema=schema,
+            env=builtin_env,
+            keys=["REDDIT_CLIENT_ID"],
+        )
+        client_secret = VENTS_CONFIG.read_keys(
+            context_paths=context_paths,
+            schema=schema,
+            env=builtin_env,
+            keys=["REDDIT_CLIENT_SECRET"],
+        )
+        user_agent = VENTS_CONFIG.read_keys(
+            context_paths=context_paths,
+            schema=schema,
+            env=builtin_env,
+            keys=["REDDIT_USER_AGENT"],
+        )
+        username = VENTS_CONFIG.read_keys(
+            context_paths=context_paths,
+            schema=schema,
+            env=builtin_env,
+            keys=["REDDIT_USERNAME"],
+        )
+        password = VENTS_CONFIG.read_keys(
+            context_paths=context_paths,
+            schema=schema,
+            env=builtin_env,
+            keys=["REDDIT_PASSWORD"],
+        )
+        return cls(
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent=user_agent,
+            username=username,
+            password=password,
+        )
+
+    def _set_session(self):
+        import praw
+
+        self._session = praw.Reddit(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            user_agent=self.user_agent,
+            username=self.username,
+            password=self.password,
+        )
+
+    def set_env_vars(self):
+        if self.client_id:
+            os.environ["REDDIT_CLIENT_ID"] = self.client_id
+        if self.client_secret:
+            os.environ["REDDIT_CLIENT_SECRET"] = self.client_secret
+        if self.user_agent:
+            os.environ["REDDIT_USER_AGENT"] = self.user_agent
+        if self.username:
+            os.environ["REDDIT_USERNAME"] = self.username
+        if self.password:
+            os.environ["REDDIT_PASSWORD"] = self.password
+
+
+class RedditRssService(BaseHttpService):
+    url: Optional[str] = "https://reddit.com/r/{subreddit}.rss"
+
+    @classmethod
+    def load_from_connection(
+        cls,
+        connection: Optional["Connection"],
+    ) -> Optional["RedditRssService"]:
+        context_paths = []
+        if connection:
+            if connection.secret and connection.secret.url:
+                context_paths.append(connection.secret.url)
+        kwargs = {}
+        url = VENTS_CONFIG.read_keys(
+            context_paths=context_paths, keys=["REDDIT_RSS_URL"]
+        )
+        if url:
+            kwargs["url"] = url
+        return cls(**kwargs)
